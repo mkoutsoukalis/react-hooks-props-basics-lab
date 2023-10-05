78a4dbd1e8b5251070c9df706f636f5a7497b122
Maria Koutsoukalis <mkoutsoukalis@outlook.com>
2023-10-05 14:54:37 -0400

    Completed

--- src/__tests__/Links.test.js
+++ src/__tests__/Links.test.js
@@ -9,13 +9,13 @@ test("renders the h3 with the text 'Links'", () => {
 });
 
 test("displays the URL of a Github link passed down as a prop", () => {
-  render(<Links github={"https://github.com/liza"} />);
-  expect(screen.queryByText("https://github.com/liza")).toBeInTheDocument();
+  render(<Links github={"https://github.com/mkoutsoukalis"} />);
+  expect(screen.queryByText("https://github.com/mkoutsoukalis")).toBeInTheDocument();
 });
 
 test("displays the URL of a Linkedin link passed down as a prop", () => {
-  render(<Links linkedin={"https://www.linkedin.com/in/liza/"} />);
+  render(<Links linkedin={"https://linkedin.com/in/maria-koutsoukalis"} />);
   expect(
-    screen.queryByText("https://www.linkedin.com/in/liza/")
+    screen.queryByText("https://linkedin.com/in/maria-koutsoukalis")
   ).toBeInTheDocument();
 });
--- src/data/user.js
+++ src/data/user.js
@@ -1,11 +1,11 @@
 const user = {
-  name: "Liza",
-  city: "New York",
+  name: "Maria",
+  city: "Atlanta",
   bio: "I made this!",
   color: "firebrick",
   links: {
-    github: "https://github.com/liza",
-    linkedin: "https://www.linkedin.com/in/liza/",
+    github: "https://github.com/mkoutsoukalis",
+    linkedin: "https://linkedin.com/in/maria-koutsoukalis",
   },
 };
 
